c64140df792d4432b66c6d6b83bbbd2e168a11ec
Michael Bernard <104996295+Upxero@users.noreply.github.com>
2025-05-08 01:32:06 +0200

    Change directus.config connection

--- directus.conf.js
+++ directus.conf.js
@@ -8,7 +8,13 @@ module.exports = {
   },
   database: {
     client: 'mysql',
-    connection: process.env.DATABASE_URL,
+    connection: {
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+    },
   },
   auth: {
     providers: {

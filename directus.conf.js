c6418bbfdaf88e1d025870fb21edf40dad2fd7b0
Michael Bernard <104996295+Upxero@users.noreply.github.com>
2025-05-07 15:29:02 +0200

    Change frontend URL

--- directus.conf.js
+++ directus.conf.js
@@ -1,11 +1,10 @@
 // directus.config.js
 module.exports = {
-    server: {
-      cors: {
-        origin: ['https://dashboard-learn-dutch.netlify.app/'], // frontend URL
-        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-        allowedHeaders: ['Content-Type', 'Authorization'],
-      },
+  server: {
+    cors: {
+      origin: ['https://dashboard-learn-dutch.netlify.app'], 
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
     },
-  };
-  
+  },
+};

9f90bd784d288305d3718ef78bea8d7b7cf808c3
Michael Bernard <104996295+Upxero@users.noreply.github.com>
2025-05-07 16:08:15 +0200

    Change config

--- directus.conf.js
+++ directus.conf.js
@@ -2,9 +2,13 @@
 module.exports = {
   server: {
     cors: {
-      origin: ['https://dashboard-learn-dutch.netlify.app'], 
+      origin: ['https://dashboard-learn-dutch.netlify.app'],
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
       allowedHeaders: ['Content-Type', 'Authorization'],
     },
+  },
+  database: {
+    client: 'mysql', 
+    connection: process.env.DATABASE_URL, 
   },
 };
